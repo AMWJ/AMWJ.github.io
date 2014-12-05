20a2f3d5ce70b6814087e51a12aaab32aec98093
AMWJ <arielj@mit.edu>
2014-12-05 12:13:53 -0500

    * Sidebar implemented * Change recipients by clicking * New Message Button

--- ChatApp/chat.js
+++ ChatApp/chat.js
@@ -1,13 +1,33 @@
 "use strict";
-var pod = crosscloud.connect();
 $(function(){
 	//Based on http://crosscloud.org/0.1.1/example/hello
-    $("#error").html("");  // clear the "Missing Javascript" error message
-	var myStatusPod=null
-	var messages=[]
+	$("#error").html("");  // clear the "Missing Javascript" error message
+	var myStatusPod=null;
+	var pod = crosscloud.connect();
 	var myStatus=""
+	var displayMessages;
+	var messages=[]
+	var displayedPosts=[]
 	var myAvailability=false
 	messages[pod.loggedInURL]=[]
+	Handlebars.registerHelper('relativeTime', function(date) {
+		return moment(date).fromNow();
+	});
+	Handlebars.registerHelper('isOwner', function(sender, options) {
+		if(sender==pod.loggedInURL)
+		{
+			return options.fn(this);
+		}
+		else
+		{
+			return options.inverse(this);
+		}
+	});
+	var newMessage = function()
+	{
+		$("#recipient").removeAttr('disabled');
+		$("#recipient").removeProp('disabled');
+	};
     var sendMessage = function () {
         
         var thisMessage = { isChatMessage: 0.15,
@@ -22,7 +42,9 @@ $(function(){
 		thisMessage.body=body
         
         pod.push(thisMessage);
-        $("#message").val("")	//Ideally this only happens when the message has been sent successfully
+        $("#message").val("");	//Ideally this only happens when the message has been sent successfully
+		$("#recipient").prop('disabled', true);
+		$("#recipient").attr('disabled', true);
     };
 	$("#send").click(sendMessage);
 	var newAvailability=function(available)
@@ -38,6 +60,14 @@ $(function(){
 			newStat["_id"]=myStatusPod
 		}
         pod.push(newStat,podStored);
+	}
+	var changeRecipient = function(recipient)
+	{
+		$("#recipient").prop('disabled', true);
+		$("#recipient").attr('disabled', true);
+		$("#recipient").val(recipient.target.innerHTML.trim());
+		displayMessages()
+		return false;
 	}
     var newStatus = function (status)
 	{
@@ -128,6 +158,10 @@ $(function(){
 			$("#recipients").append(newRecipientOption(recipient));
 
 		});
+		var theTemplateScript = $("#recipientListItem").html(); 
+		var theTemplate = Handlebars.compile (theTemplateScript); 
+		$("#contactList").html(theTemplate(rs));
+
 	}
 	var newRecipientOption=function(recipient)
 	{
@@ -171,14 +205,37 @@ $(function(){
 			}
 		});
 	}
-    var displayMessages = function ()
+    displayMessages = function ()
 	{
 		$("#out").html("");
 		var table = $("#results");
 		var currentRecipient=$("#recipient").val()
 		displayedPosts=messages[currentRecipient]||[]
 		displayedPosts.sort(function(a,b){return a.when<b.when?-1:(a.when===b.when?0:1)});
-		$("#it").click()
+		var isBottom=false;
+		if ($(window).height() + $(window).scrollTop() == $(document).height())
+		{
+				isBottom=true;
+		}	
+		if(currentRecipient=="")
+		{
+			$("#send").prop('disabled', true);
+			$("#send").attr('disabled', true);
+		}
+		else
+		{
+			$("#send").prop('disabled', false);
+			$("#send").attr('disabled', false);
+		}
+		//Databind messages
+		var theTemplateScript = $("#chatHistory").html(); 
+		var theTemplate = Handlebars.compile (theTemplateScript); 
+		$(".messages").html(theTemplate(displayedPosts));
+		$(".contact").click(changeRecipient)
+		if(isBottom)
+		{
+			window.scrollTo(0,document.body.scrollHeight);
+		}
     };
 
     pod.onLogin(function () {
@@ -204,12 +261,14 @@ $(function(){
             .filter( { isChatStatus:0.15 } )
             .onAllResults(updateStatuses)
             .start();
-		$("#recipient").on("input",displayMessages);
+		//$("#recipient").on("input",displayMessages);
     });
 	var refreshList=function()
 	{
 		
 	}
+    $("#newMessage").click(newMessage);
+
 });
 function recipientList(recipients)
 {
